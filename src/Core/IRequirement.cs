e7520ac5971a26216de10c2586960c62f0c16110
xd-loler <77643169+xd-loler@users.noreply.github.com>
2021-09-05 18:11:15 +0200

    Clarifications about IRequirement.

--- src/Core/IRequirement.cs
+++ src/Core/IRequirement.cs
@@ -8,13 +8,18 @@
         string Name { get; }
 
         /// <summary>
+        /// <para>
         /// Takes original string and current index
         /// e.g "abc" and 1
         /// and has to return whether current requirement is fulfilled and
         /// at which index should we move after this requirement.
-        /// For example: We have string "abc" and current index "1"
-        /// And our requirement tries to find string "b"
-        /// Since "b" is at current index, then we return success and the next index within bounds - 2.
+        /// </para>
+        /// <para>
+        /// For example: We have string "abc" and current index: 2
+        /// And our requirement tries to find string "c"
+        /// Since "c" is at current index, then we return success and the next index: 3.
+        /// The fact that it'll be out of bounds will be handled by evaluator.
+        /// </para>
         /// </summary>
         /// <param name="original">Full, original string</param>
         /// <param name="index">Current index within original string which indicates start place for our requirement</param>
